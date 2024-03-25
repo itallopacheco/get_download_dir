9ab6ce2844e0d7c3f3fe8aa09d2ad2dd18e5d6f6
itallopacheco <itallopss@dcomp.ufs.br>
2024-03-24 21:34:43 -0300

    teste

--- lib/download_directory.dart
+++ lib/download_directory.dart
@@ -6,3 +6,16 @@ class DownloadDirectory {
     return downloadsDirectory?.path;
   }
 }
+
+//TESTE
+void main() async {
+  // Obtém o diretório de downloads
+  String? downloadDirectory = await DownloadDirectory.getDownloadDirectory();
+
+  // Verifica se o diretório de downloads foi encontrado
+  if (downloadDirectory != null) {
+    print('O diretório de downloads é: $downloadDirectory');
+  } else {
+    print('Não foi possível encontrar o diretório de downloads.');
+  }
+}
