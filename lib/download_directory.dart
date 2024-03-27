8551e37e372e3f57c700788b0a97990780e7ed8a
itallopacheco <itallopss@dcomp.ufs.br>
2024-03-27 17:46:24 -0300

    teste

--- lib/download_directory.dart
+++ lib/download_directory.dart
@@ -1,18 +1,11 @@
 import 'package:path_provider/path_provider.dart';
 
 class DownloadDirectory {
-
   static Future<String?> getDownloadDirectory() async {
     final downloadsDirectory = await getDownloadsDirectory();
-    return downloadsDirectory == null ? null: downloadsDirectory.path;
+    return downloadsDirectory == null ? null : downloadsDirectory.path;
   }
 }
-static Future<Directory?> downloadsDirectory({String? dirType}) async {
-    final String? path = await _channel
-        .invokeMethod('getDownloadsDirectory', {'directoryType': dirType});
-
-    return path == null ? null : Directory(path);
-  }
 
 //TESTE
 void main() async {
